86715cff8b5940c9cfa1b8a5a745532d6a56850b
Ashik Raj <ashikraj.78@gmail.com>
2023-10-27 10:08:26 +0530

    removing the scrol icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 import {styles} from "../style"
 import { ComputersCanvas } from "./canvas";
@@ -18,9 +19,9 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+      {/* <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center cursor-pointer">
+        <a href="#about" className="cursor-pointer">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 cursor-pointer">
             <motion.dev 
               animate={{y:[0, 24, 0]}}
               transition={{
@@ -32,7 +33,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   )
 }
